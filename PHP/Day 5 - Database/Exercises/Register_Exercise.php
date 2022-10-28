5c83f726317f781c355f61be347b546570731051
Simon <fctinstean@gmail.com>
2022-10-28 12:06:13 +0200

    artist details

---
+++ PHP/Day 5 - Database/Exercises/Register_Exercise.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+
+    Exercise :
+
+    In this exercise, you'll have to create the register process.
+
+    - Step 1 :
+        Create a 'register.php' file
+        This file contains a form with those input :
+            * username
+            * email
+            * password
+            * confirm password
+            * submit btn
+
+    - Step 2 :
+        If the form is submitted, you have to check :
+            * username, mail and password must not be empty !
+            * mail must be a valid one (with @symbol)
+            * passwords must be the same 
+        
+        Display a different message for each validation/error.
+
+    - Step 3 :
+        If everything is good, insert the new user in the Users table.
+
+        You can make sure it worked by looking at PHPMYADMIN directly after submitting the form.
+
+    */
--- spotify_exercise/artist-details.php
+++ spotify_exercise/artist-details.php
@@ -1,3 +1,56 @@
 <?php
 
-echo "my id : " . $_GET['id'];
+$conn = mysqli_connect('localhost', 'root', '', 'spotify');
+$query = "SELECT *, COUNT(*) as NbSongs
+FROM artists a
+INNER JOIN songs s ON s.artist_id = a.id
+WHERE a.id = " . $_GET['id'];
+
+// echo "query : " . $query;
+
+
+$result = mysqli_query($conn, $query);
+$artist = mysqli_fetch_assoc($result);
+mysqli_close($conn);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php require_once 'nav.html'; ?>
+
+    <h1>Artist Details</h1>
+
+    <p>
+        <strong>Name : </strong>
+        <?= $artist['name']; ?>
+    </p>
+    <p>
+        <strong>Biography : </strong>
+        <?= $artist['biography']; ?>
+    </p>
+    <p>
+        <strong>Birth year : </strong>
+        <?= $artist['birth_year']; ?>
+    </p>
+
+    <img src="./assets/img/<?= $artist['poster']; ?>" width="200px">
+    <hr>
+
+    <p>
+        <strong>Number of songs : </strong>
+        <?= $artist['NbSongs']; ?>
+    </p>
+
+</body>
+
+</html>

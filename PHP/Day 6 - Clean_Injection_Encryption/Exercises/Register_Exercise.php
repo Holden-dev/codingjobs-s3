41fa594d912764d5d10d56ed08fbf891f0faabff
Simon <fctinstean@gmail.com>
2022-10-28 16:01:34 +0200

    Register exercise next part

---
+++ PHP/Day 6 - Clean_Injection_Encryption/Exercises/Register_Exercise.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+
+Continue Register from Spotify exercise
+
+- Step 1 :
+
+Protect your input by removing white spaces and HTML tags.
+
+-- Step 2 : 
+
+Hash the password before saving in the DB
+
+*/

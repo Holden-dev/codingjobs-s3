5e55e6b59cb5b79080d920686d96392d3d3146bd
Simon <fctinstean@gmail.com>
2022-11-03 16:06:53 +0100

    Update Upload_Exercise.php

--- PHP/Day 7 - Files_and_Upload/Exercises/Upload_Exercise.php
+++ PHP/Day 7 - Files_and_Upload/Exercises/Upload_Exercise.php
@@ -9,7 +9,8 @@ Let's continue movie project
 
 - Part 2 :
     Edit 'register.php', to allow the user upload a picture.
-
+    Profile picture name must be same as user's name
+    
 - Part 3 :
     Display the profile picture on the account page.
 */

e3500648eab82db1ee8408ce6b3adc758c6b1dff
Simon <fctinstean@gmail.com>
2022-11-04 14:25:00 +0100

    AJAX exercise 3

---
+++ PHP/Day 8 - AJAX/Exercises/PHP_Ajax_Exercise_3.php
@@ -0,0 +1,30 @@
+<?php 
+
+/*
+
+	-- Exercise : 
+
+	In this exercise, you will use the 'movie_db' project.
+
+	- Step 1 :
+		Edit 'insert-movie.php' page.
+		You have to remove the code which retrieve directors list from DB.
+		
+		Create a new file named 'get-directors.php' and place all the code to retrieve directors inside this one.
+
+	- Step 2 :
+		In 'insert-movie.php', you should now make an AJAX call to retrieve the directors list from 'get-directors.php' file.
+		
+		Make sure you push the result inside the <select> element.
+		
+	- Step 3 :
+		Edit 'insert-movie.php' page again.
+
+		Remove the code that take care of input validations + insert and place this code in a new file 'new-movie.php'
+
+		When clicking on submit button you must do an AJAX call to the 'new-movie.php' file.
+
+		
+	CONGRATZ, YOU'VE USED AJAX TO INSERT IN THE DATABASE !
+
+*/

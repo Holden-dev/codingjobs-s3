811a37ff53ac62cc4a9c5e5c5c7fda91d11a4fa8
Simon <fctinstean@gmail.com>
2022-11-04 11:21:30 +0100

    AJAX Exercise

---
+++ PHP/Day 8 - AJAX/Exercises/PHP_Ajax_Exercise_1.php
@@ -0,0 +1,19 @@
+<?php 
+
+/*
+	-- Exercise : 
+
+	In this exercise, you will use the 'movie_db' project.
+
+	- Step 1 :
+		Create two pages: 'movies.html' and 'get-movies.php'
+    
+    - Step 2 : 
+        'get-movies.php' should query the database to get all the movies.
+    
+    - Step 3 :
+        When you reach movies.html, this page should display all the movies.
+        To perfom this action, you must use AJAX.
+        AJAX should get the data from 'get-movies.php' and display it on the page.
+
+*/
---
+++ PHP/Day 8 - AJAX/Exercises/PHP_Ajax_Exercise_2.php
@@ -0,0 +1,16 @@
+<?php 
+
+/*
+	-- Exercise : 
+
+	In this exercise, you will use the 'movie_db' database.
+
+	Edit the 'movies.html' page.
+		In this file, create a form with only one input (search textbox).
+		This input will be use to search for a movie.
+		
+		Each time the user press a key, results are shown/updated on the page.
+
+    To achieve this you should create a php file ('search-movies.php'). 
+		This PHP file looks for a movie matching specific characters and display the movies matching.
+*/

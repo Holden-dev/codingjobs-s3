e6ac64dc9c7fd770a9204152910705498482639f
Simon <fctinstean@gmail.com>
2022-11-07 13:47:03 +0100

    Create instructions.php

---
+++ PHP/Day 9 - Pagination_Cart_Recursive/Exercises/Cart_Exercise/instructions.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+
+    - Step 1 : 
+    Edit 'books.php' page.
+    For each book, add a button/link 'add to cart'
+
+    When you click on the button, it should add the book to the cart.
+
+    - Step 2 :
+
+    Edit nav.php
+    You should be able to see, in the nav, the number of books inside your cart.
+    When you add a book to the cart, it should update this number.
+
+    - Step 3 : 
+
+    Edit 'cart.php' page.
+    Display every books from your cart and display total price.
+    If cart is empty display a message "your cart is empty"
+
+*/

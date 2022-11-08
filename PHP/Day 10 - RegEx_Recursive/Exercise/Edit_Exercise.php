57172fcc0b2b3cb7746115b600b173d1b6bfd892
Simon <fctinstean@gmail.com>
2022-11-08 09:50:54 +0100

    Exercise

---
+++ PHP/Day 10 - RegEx_Recursive/Exercise/Edit_Exercise.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+
+Continue 'Movie' project.
+
+- Step 1 :
+
+Create 'edit-movie.php' page.
+This page contains a form to edit a specific movie.
+
+The form will be populated with movie's informations when you reach the page.
+
+To know which movie to edit, use GET method to retrieve the id of the movie.
+
+- Step 2 :
+
+When you submit, execute UPDATE query.
+
+*/

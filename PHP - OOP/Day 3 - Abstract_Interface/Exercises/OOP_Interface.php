baf5c4e4ba0f7735707a6ee09afc8cd23f10934d
Simon <fctinstean@gmail.com>
2022-11-18 16:21:23 +0100

    Create OOP_Interface.php

---
+++ PHP - OOP/Day 3 - Abstract_Interface/Exercises/OOP_Interface.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+	Using the Animal/Robot/Human exercise.
+
+	- Step 1 :
+		Think about the nature of your classes. 
+		Some of them share the same role and has to implement interface.
+
+		Create this interface and fix the matching classes.
+
+	- Step 2 :
+
+		In a main program (client) :
+
+		1. Create an array of robots and humans
+		2. Create a script where :
+			- Loop through the array
+			- Each time the current element should work

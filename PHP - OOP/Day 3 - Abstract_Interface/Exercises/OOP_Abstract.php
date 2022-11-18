38152ebfa145f35424df3f6b45894a2f770f759f
Simon <fctinstean@gmail.com>
2022-11-18 14:34:52 +0100

    Create OOP_Abstract.php

---
+++ PHP - OOP/Day 3 - Abstract_Interface/Exercises/OOP_Abstract.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+	Using the Animal/Robot/Human exercise.
+
+	- Step 1 :
+		Think about the nature of your classes. At least one of them will never be instantiate...
+		You need to turn them into abstract class.
+		Maybe change some methods to make it abstract methods.
+		
+		Tips : If you pay attention, many of our classes can 'talk' or 'make a sound'
+
+	- Step 2 :
+
+		In a main program (client) :
+
+		1. Create an array of animals and humans (cats, dogs or humans)
+		2. Create a script where :
+			- Loop through the array
+			- Each time the current element should make a sound
+
+*/

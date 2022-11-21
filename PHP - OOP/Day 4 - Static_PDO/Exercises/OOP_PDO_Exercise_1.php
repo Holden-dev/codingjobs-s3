8e174129778d192842b87ede07d414122e976187
Simon <fctinstean@gmail.com>
2022-11-21 11:26:58 +0100

    Create OOP_PDO_Exercise_1.php

---
+++ PHP - OOP/Day 4 - Static_PDO/Exercises/OOP_PDO_Exercise_1.php
@@ -0,0 +1,15 @@
+<?php
+
+/* 
+
+-- Exercise : 
+
+	Let's go back to 'spotify'
+
+	Create a page 'songs.php' where you should see all songs.
+	Use PDO.
+
+	Create a page 'song-details.php' that show detail page of a specific song.
+	Use PDO & Prepared statement here.
+
+*/

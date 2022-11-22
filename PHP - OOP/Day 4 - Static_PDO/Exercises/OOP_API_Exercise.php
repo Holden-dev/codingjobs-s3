da5f1348c68bbfd6ad78716f4b0a0f9a1c34e7e4
Simon <fctinstean@gmail.com>
2022-11-22 09:22:31 +0100

    Create OOP_API_Exercise.php

---
+++ PHP - OOP/Day 4 - Static_PDO/Exercises/OOP_API_Exercise.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+
+In this exercise, we will continue FLOWER EXERCISE
+
+--- Exercise 1
+
+- Step 1 : Create the FlowerAPIManager class.
+It will contains one function findAll().
+
+In this function, return all the flowers in a JSON format.
+
+- Step 2 : Add a new function find($id)
+In this function, return a specific flower as JSON format.
+
+- Step 3 : Create two different pages.
+Those page will be use to display json for all flowers or one specific flower (using 'findAll' et 'find')
+
+*/

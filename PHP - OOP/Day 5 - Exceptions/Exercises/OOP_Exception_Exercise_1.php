1114ee4d90d68e993ec32dbfdc11c9df50f11d91
Simon <fctinstean@gmail.com>
2022-11-22 14:05:30 +0100

    exception exercises

---
+++ PHP - OOP/Day 5 - Exceptions/Exercises/OOP_Exception_Exercise_1.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+
+Remember exercises on animals, humans, robots....
+Unfortunately, when employees work, sometimes some of these employees are injured: it is known as work accident.
+
+There is one in four chance that a workplace accident will occur.
+When this happens, you have to remove the employee from the worker list (client) and put him on a list of injured workers.
+
+A robot can't be injured.
+
+Tips : When a human works, it is necessary to use a random and to raise an exception 'work accident'.
+
+*/
---
+++ PHP - OOP/Day 5 - Exceptions/Exercises/OOP_Exception_Exercise_2.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+    
+    For this exercise, you should use PHP in console.
+	You can use readline() function to expect an input from the user.
+	https://www.php.net/manual/fr/function.readline.php
+
+	- Part 1
+		A program asks the user for a file name.
+		For now, the program does nothing else.
+	- Part 2
+		Now, when the user enters the name of the file, the program will read this file and then display the content.
+		You must handle the case where the file does not exist (part 3).
+	- Part 3
+		In case the file does not exist, give the user an additional chance to enter a new file name.
+		The user has 3 chances for enter a valid filename.
+
+*/

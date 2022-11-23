ba3f539cb7f61aece56584e38f7f9ca2206a5066
InsteanAzeros <fctinstean@gmail.com>
2022-11-23 10:40:31 +0100

    Email and composer

---
+++ PHP - OOP/Day 6 - Namespace_Mail/Example files/send_email.php
@@ -0,0 +1,41 @@
+<?php
+
+//Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+//Load Composer's autoloader
+require 'vendor/autoload.php';
+
+//Create an instance; passing `true` enables exceptions
+$mail = new PHPMailer(true);
+
+try {
+    //Server settings
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp-mail.outlook.com';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'simon-bertrand@live.fr';                     //SMTP username
+    $mail->Password   = 'mypassword';                               //SMTP password
+    // $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;            //Enable implicit TLS encryption
+    $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+    //Recipients
+    $mail->setFrom('simon-bertrand@live.fr', 'Simon Bertrand');
+    $mail->addAddress('joe@example.net', 'Joe');     //Add a recipient
+
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = 'Here is the subject';
+    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
---
+++ PHP - OOP/Day 6 - Namespace_Mail/Exercise/Email_Exercise.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+    Create a contact form asking for : 
+        - Name (fullname)*
+        - Phone number
+        - Email address*
+        - Subject
+        - Message*
+    When user submit the form you have to :
+        1. Check if the mandatory fields are not empty (*)
+        2. Display error messages if not ok.
+        3. Send mail if everything is ok.
+            Display a message on the page saying 'mail was sent'
+*/

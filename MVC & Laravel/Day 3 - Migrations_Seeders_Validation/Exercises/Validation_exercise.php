50e78f2f87c4a330f410d79395c05e153471bce7
Simon <fctinstean@gmail.com>
2022-11-29 15:50:34 +0100

    validations

---
+++ MVC & Laravel/Day 3 - Migrations_Seeders_Validation/Exercises/Validation_exercise.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+
+Get back to our Flower Exercise.
+
+You should improve your project using validations ! 
+
+Resource : https://laravel.com/docs/9.x/validation
+
+Step 1 :
+
+    When the form to create new flower is submitted, you have to check :
+        - name is required, min 3 characters and max 30 characters
+        - price is required and have to be numeric between 2 and 100€
+    
+    Error messages should be display in red on the page;
+    
+    Implements validations on the EDIT form also.
+
+
+
+Step 2 - Bonus :
+
+    You should customize the error message.
+    If the title is empty, you should display 'Title is mandatory !'
+    If the price is not a numeric value OR is not between 2 and 100 it should display 'Price must be numeric between 2 and 100e'
+
+    For this step, you have to read this section : https://laravel.com/docs/9.x/validation#customizing-the-error-messages
+    
+*/

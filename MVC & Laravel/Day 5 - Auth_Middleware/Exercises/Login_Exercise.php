463ae85ab37f024ff0d86cc87100331db122dc12
Simon <fctinstean@gmail.com>
2022-12-01 15:24:41 +0100

    login

---
+++ MVC & Laravel/Day 5 - Auth_Middleware/Exercises/Login_Exercise.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+
+Take care of the login form.
+When user submit login, you have to :
+    - Check if the user exists
+    - If it exists, save the email in the session
+    - Redirect to account page (or any other page)
+
+*/

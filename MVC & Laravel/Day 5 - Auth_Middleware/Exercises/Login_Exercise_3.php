cd2f100c9bd34117cbdaca4e33e7816f1f9bc983
Simon <fctinstean@gmail.com>
2022-12-05 11:40:51 +0100

    Login

---
+++ MVC & Laravel/Day 5 - Auth_Middleware/Exercises/Login_Exercise_3.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+
+Continue Login exercise.
+
+- Step 1 :
+
+    Create logout button.
+    When you click, it should logout the user and redirect to 'login' page.
+
+- Step 2 :
+
+    If you are not loggued in, display 'login'/'register' in the navbar.
+    If you are loggued in, display 'logout' and hide 'login'/'register' from the navbar.
+
+*/

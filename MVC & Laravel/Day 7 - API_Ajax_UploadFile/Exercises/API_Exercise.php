1222a4cfcb3dbab81fe3b82aab67ece2daac0dfa
Simon <fctinstean@gmail.com>
2022-12-06 11:13:32 +0100

    api

---
+++ MVC & Laravel/Day 7 - API_Ajax_UploadFile/Exercises/API_Exercise.php
@@ -0,0 +1,27 @@
+<?php 
+
+/*
+
+Continue on the Flower Exercise.
+
+-- Step 1 :
+
+    Create an APIController
+
+-- Step 2 :
+
+    Create routes to be able to retrieve json :
+        - List all flowers
+
+        - Filter by min price
+        For example /flowers/minprice=2
+
+        - Filter by max price
+        For example /flowers/maxprice=30
+
+        - Filter by min AND max price
+        For example /flowers/minprice=2&maxprice=30
+
+        - Filter by name : Give you all flowers looking like the name
+        For example /flowers/name=tulip
+*/
